b4da335888b1fd65661c769209983d2040179b9b
Anandrajoriya <anandrajoriya03@gmail.com>
2025-06-11 22:06:46 +0530

    find largest

--- practice/findLargest.js
+++ practice/findLargest.js
@@ -1,12 +1,12 @@
-function findLargest(arr){
-    let max=arr[0];
+function findLargest(arr) {
+  let max = arr[0];
 
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]>max){
-            max=arr[i]
-        }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
-    return max
+  }
+  return max;
 }
 
-console.log(findLargest([1,24,3,4]));
+console.log(findLargest([4, 2, 3, 4]));
