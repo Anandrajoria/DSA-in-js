3b56c3c5167d3a53a3790750147781f9fc059f4d
Anandrajoriya <anandrajoriya03@gmail.com>
2025-06-12 14:02:28 +0530

    insertion sort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,16 +1,31 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let cur = arr[i];
-    let j = i - 1;
+/*
+STEPS:
+1: consider 2nd element , arr[1]- start yahi se hoga
+2: arr[j]> cur - swapping hogi
+ */
 
-    while (j >= 0 && arr[j] > cur) {
-      arr[j + 1] = arr[j];
+function insertionSort(){
+  for(let i=1;i<arr.length;i++){  // outer loop that goes toward right side
+    let cur=arr[i];  //store the 2nd element for comparison
+
+    let j=i-1;
+
+    while(j>=0 && arr[j]>cur){ 
+      arr[j+1]=arr[j]
       j--;
     }
-    arr[j + 1] = cur;
+    arr[j+1]=cur   
   }
-  return arr;
+  return arr
 }
 
-let res = insertionSort([8, 2, 4, 1, 3]);
-console.log(res);
+const arr=[7,8,2,4,1]
+console.log(insertionSort(arr));
+
+
+/*
+
+Best Case (Already Sorted)	O(n)
+Average Case	O(n²)
+Worst Case (Reverse Sorted)	O(n²)
+ */
