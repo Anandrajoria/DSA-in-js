d6ab0bc45078745a038c53c2a9c8324303440816
Anandrajoriya <anandrajoriya03@gmail.com>
2025-06-26 18:34:32 +0530

    pattern 1

--- pattern/pattern1.js
+++ pattern/pattern1.js
@@ -1,21 +1,12 @@
-// Function to determine grade based on marks
-function calculateGrade(marks) {
-  if (marks >= 90) {
-    return "Grade A";
-  } else if (marks >= 70) {
-    return "Grade B";
-  } else if (marks >= 50) {
-    return "Grade C";
-  } else if (marks >= 35) {
-    return "Grade D";
-  } else {
-    return "Fail";
+function pattern1(n) {
+  for (let i = 0; i <= n; i++) {
+    let str = "";
+
+    for (let col = 1; col <= n; col++) {
+      str += "*";
+    }
+    console.log(str);
   }
 }
 
-// Test cases
-console.log("Marks: 95 ->", calculateGrade(95)); // Grade A
-console.log("Marks: 75 ->", calculateGrade(75)); // Grade B
-console.log("Marks: 55 ->", calculateGrade(55)); // Grade C
-console.log("Marks: 40 ->", calculateGrade(40)); // Grade D
-console.log("Marks: 30 ->", calculateGrade(30)); // Fail
+pattern1(4)
