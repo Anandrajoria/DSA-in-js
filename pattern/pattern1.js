8fc6e5a5c0cae3f9ac744024dfa8302a0656f94a
Anandrajoriya <anandrajoriya03@gmail.com>
2025-06-27 19:27:58 +0530

    pattern 1

--- pattern/pattern1.js
+++ pattern/pattern1.js
@@ -1,12 +1,15 @@
-function pattern1(n) {
-  for (let i = 0; i <= n; i++) {
-    let str = "";
+/*  //! below code is used for pattern printing
+let prompt=require('prompt-sync')()
+let a = prompt("enter a number ");
+process.stdout.write(a)
+*/
 
-    for (let col = 1; col <= n; col++) {
-      str += "*";
-    }
-    console.log(str);
+let prompt = require("prompt-sync")();
+let n = prompt("enter a number ");
+
+for (let i = 0; i <= n; i++) {
+  for (let j = 0; j < n; j++) {
+    process.stdout.write("*");
   }
+  console.log();
 }
-
-pattern1(4)
