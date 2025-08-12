b09ebdc13c4a9577514b40d474899a98fd0733ea
Anandrajoriya <anandrajoriya03@gmail.com>
2025-08-12 11:50:18 +0530

    finding the second largest

--- WarmUp/secondLargest.js
+++ WarmUp/secondLargest.js
@@ -1,23 +1,24 @@
 // find the second largest in an array
 
 function secondLargest(arr) {
-    if(arr.length<2){
-        return null
+  if (arr.length < 2) {
+    return null;
+  }
+  let firstLargest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > firstLargest) {
+      secondLargest = firstLargest;
+      firstLargest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] != firstLargest) {
+      secondLargest = arr[i];
     }
-    let firstLargest=-Infinity
-    let secondLargest=-Infinity
-    
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]>firstLargest){
-            secondLargest=firstLargest
-            firstLargest=arr[i]
-        }else if(arr[i]>secondLargest){
-            secondLargest=arr[i]
-        }
-    }
-    return secondLargest
+  }
+    return secondLargest === -Infinity ? "No second largest found" : secondLargest;
+
 }
 
-let arr=[4,7,8,2,9,5,2]
-let result=secondLargest(arr)
+let arr = [2,2,2,2,2];
+let result = secondLargest(arr);
 console.log(result);
