2e7dd609488e54f56052473f8ea9d0f613e7dd56
Aditya Rajoriya <anandrajoriya03@gmail.com>
2026-06-02 22:22:15 +0530

    practice

--- Arrays/27_removeEle.js
+++ Arrays/27_removeEle.js
@@ -1,15 +1,16 @@
-let arr=[2,3,3,2]
-let val=3
+let arr = [2, 3, 3, 2];
+let val = 3;
 
-function removeEle(arr,val){
-    let x=0
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]!=val){
-            arr[x]=arr[i]
-            x+=1;
-        }
+function removeEle(arr, val) {
+  let x = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] != val) {
+      arr[x] = arr[i];
+      x += 1;
     }
-    return x
+  }
+  return x;
 }
 
-console.log(removeEle(arr,val));
+let count = removeEle(arr, val);
+console.log(arr.slice(0, count));
